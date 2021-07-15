1c6915f67a3babc831d930d76d78dfa172080fdc
AimeAriadne <aimeariadne@hotmail.com>
2021-07-15 11:13:27 -0300

    adiciona primeiro commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// swiper
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination'
+  }
+})
+
+function saveEmail() {
+  let email = document.getElementById('email').value
+  if (email !== '') {
+    localStorage.setItem('savedEmail', email)
+    alert('Email salvo com sucesso')
+  } else {
+    alert('Preencha o campo e-mail')
+  }
+}
